0642999e0a6241aff5bf292c6082e20cdaf65e89
HenryJCM <henjacolonia17@gmail.com>
2017-12-19 13:02:45 -0500

    pruebas query

--- src/main/java/pe/edu/sistemas/sisrepweb/repository/CursoPeriodoRepository.java
+++ src/main/java/pe/edu/sistemas/sisrepweb/repository/CursoPeriodoRepository.java
@@ -1,19 +1,25 @@
 package pe.edu.sistemas.sisrepweb.repository;
 
 import java.io.Serializable;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import pe.edu.sistemas.sisrepweb.domain.CursoPeriodo;
 
 @Repository
+@Transactional
 public interface CursoPeriodoRepository extends JpaRepository<CursoPeriodo, Serializable>{
 	
-	/*@Query("SELECT hc.grupo.cursoPeriodo FROM HorarioClase as hc "
-			+ "WHERE hc.horarioClasePeriodo = ?1 "
-			+ "AND hc.docente.iddocente = ?2"
-			+ "AND hc.grupo.cursoPeriodo.cursoConjunto.cursoBase.plan.escuela.idescuela = ?3")
-	List<CursoPeriodo> obtenerCursosxDocenteyEscuela(String periodo, String idDocente, String idEscuela);
-	*/
+	@Query(value = "SELECT cp FROM HorarioClase hc, hc.grupo g, g.cursoPeriodo cp, cp.cursoConjunto cc, cc.cursoBase cb,"
+			+ "cb.plan p, p.escuela e, hc.docente d "
+			+ "WHERE hc.horarioClasePeriodo = :periodo "
+			+ "AND d.iddocente = :idDocente")
+	List<CursoPeriodo> obtenerCursosxDocenteyEscuela(@Param("periodo") String periodo,
+			@Param("idDocente") Integer idDocente);
+	
 }
